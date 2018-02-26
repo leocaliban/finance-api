0d23485489011c3f54b1bfeb3d2def632820fb17
Leocaliban <leocaliban@gmail.com>
2018-02-26 18:40:07 -0300

    Aplica a migração de dados com o Flyway

--- finance-api/src/main/java/com/leocaliban/finance/api/FinanceApiApplication.java
+++ finance-api/src/main/java/com/leocaliban/finance/api/FinanceApiApplication.java
@@ -3,6 +3,12 @@ package com.leocaliban.finance.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe main para a inicialização da aplicação Spring
+ * @author Leocaliban
+ *
+ * 26 de fev de 2018
+ */
 @SpringBootApplication
 public class FinanceApiApplication {
 
