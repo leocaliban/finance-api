fc66bb2cd3a4d786e2b4db5e15599d0a0800ea39
Leocaliban <leocaliban@gmail.com>
2019-02-25 01:55:01 -0300

    Implementa o registro de logs no envio de e-mail

--- finance-api/src/main/java/com/leocaliban/finance/api/service/LancamentoService.java
+++ finance-api/src/main/java/com/leocaliban/finance/api/service/LancamentoService.java
@@ -8,6 +8,8 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -44,6 +46,8 @@ import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 public class LancamentoService {
 	
 	private static final String DESTINATARIOS = "ROLE_PESQUISAR_LANCAMENTO";
+	
+	private static final Logger logger = LoggerFactory.getLogger(LancamentoService.class);
 	
 	@Autowired 
 	private PessoaRepository pessoaRepository;
@@ -167,14 +171,28 @@ public class LancamentoService {
 		}
 	}
 	
-	 //@Scheduled(fixedDelay = 1000 * 60 * 30) 
+	@Scheduled(fixedDelay = 1000 * 60 * 30) 
 	//@Scheduled(cron = "0 0 6 * * *")
 	public void notificarLancamentosVencidos() {
+		if(logger.isDebugEnabled()) {
+			logger.debug("Preparando envio e-mails de Lançamentos vencidos.");
+		}
 		List<Lancamento>vencidos = lancamentoRepository.findByDataVencimentoLessThanEqualAndDataPagamentoIsNull(LocalDate.now());
 		
+		if(vencidos.isEmpty()) {
+			logger.info("Sem lançamentos vencidos");
+			return;
+		}
+		
+		logger.info("Existem {} lançamentos vencidos.", vencidos.size());
 		List<Usuario>destinatarios = usuarioRepository.findByPermissoesDescricao(DESTINATARIOS);
 		
+		if(destinatarios.isEmpty()) {
+			logger.warn("Existem lançamentos vencidos, mas o sistema não encontrou destinatários.");
+			return;
+		}
 		mailer.notificarLancamentosVencidos(vencidos, destinatarios);
+		logger.info("O e-mail de Lançamentos vencidos foi enviado com sucesso!");
 	}
 
 	
