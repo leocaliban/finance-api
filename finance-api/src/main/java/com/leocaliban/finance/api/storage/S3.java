b514dd4eac48c4fabe1c0ca4b070862f559ac292
Leocaliban <leocaliban@gmail.com>
2019-02-26 21:42:11 -0300

    Implementa a remoção e substituição de um anexo

--- finance-api/src/main/java/com/leocaliban/finance/api/storage/S3.java
+++ finance-api/src/main/java/com/leocaliban/finance/api/storage/S3.java
@@ -9,10 +9,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.AccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.GroupGrantee;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.ObjectTagging;
@@ -75,6 +77,22 @@ public class S3 {
 				objeto, new ObjectTagging(Collections.emptyList()));
 
 		amazons3.setObjectTagging(setObjectTaggingRequest);
+	}
+
+	public void remover(String objeto) {
+		DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(property.getS3().getBucket(), objeto);
+		
+		amazons3.deleteObject(deleteObjectRequest);
+
+	}
+
+	public void substituir(String objetoAntigo, String objetoNovo) {
+		if(StringUtils.hasText(objetoAntigo)) {
+			this.remover(objetoAntigo);
+		}
+		
+		salvar(objetoNovo);
+		
 	}
 
 }
